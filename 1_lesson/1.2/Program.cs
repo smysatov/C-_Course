b2c363e4886c861de060d6cb6007890e8631ffe7
Sergey <smysatov@gmail.com>
2022-11-21 22:50:09 +0300

    Added task 1.3

--- 1_lesson/1.2/Program.cs
+++ 1_lesson/1.2/Program.cs
@@ -1,2 +1,21 @@
 ﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+Console.WriteLine("Enter number"); int y = int.Parse(Console.ReadLine()); if ( y == 1)  {     Console.WriteLine("Понедельник"); } else if (y == 2) {     Console.WriteLine("Вторник"); } else if (y == 3) {     Console.WriteLine("Cреда"); } else if (y == 4)
+{
+    Console.WriteLine("Четверг");
+}
+else if (y == 5)
+{
+    Console.WriteLine("Пятница");
+}
+else if (y == 6)
+{
+    Console.WriteLine("Суббота");
+}
+else if (y == 7)
+{
+    Console.WriteLine("Воскресенье");
+}
+else
+{
+    Console.WriteLine("error");
+}
