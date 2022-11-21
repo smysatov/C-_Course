dd0965c139a125616c4c2b05b929cd148472d71e
Sergey <smysatov@gmail.com>
2022-11-21 21:56:35 +0300

    Added task 1.1

---
+++ 1_lesson/1.1/Program.cs
@@ -0,0 +1,15 @@
+﻿// вход 2 числа, проверка, является ли 1ое квадратом 2го
+
+Console.WriteLine("Enter first number");
+int num1 = int.Parse(Console.ReadLine());
+
+Console.WriteLine("Enter second number");
+int num2 = int.Parse(Console.ReadLine());
+
+if(num2 == num1*num1)
+{
+    Console.WriteLine("YES");
+}
+
+else
+    Console.WriteLine("NO");
