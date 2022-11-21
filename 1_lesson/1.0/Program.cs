2811eb357dbdbfe0032cac21fca50d24ddd808b7
Sergey <smysatov@gmail.com>
2022-11-21 21:33:36 +0300

    Added task 1.0

--- 1_lesson/1.0/Program.cs
+++ 1_lesson/1.0/Program.cs
@@ -1,2 +1,7 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// Программа, на вход принимает число и выдает его квадрат
+
+Console.WriteLine("Write a number: ");
+
+int num = int.Parse(Console.ReadLine());
+
+Console.WriteLine(num*num);
