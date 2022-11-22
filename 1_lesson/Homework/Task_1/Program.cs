58846297c025c283a755ccd5e78af411529bd9ca
Sergey <smysatov@gmail.com>
2022-11-22 13:41:19 +0300

    Added homework task 1

---
+++ 1_lesson/Homework/Task_1/Program.cs
@@ -0,0 +1,25 @@
+﻿// На вход принимает 2 числа и выдает, какое число большее,
+// а какое меньшее.
+
+Console.WriteLine("Enter first number");
+int num1 = int.Parse(Console.ReadLine());
+
+Console.WriteLine("Enter second number");
+int num2 = int.Parse(Console.ReadLine());
+
+if(num1 < num2)
+{
+    Console.Write("max = ");
+    Console.Write(num2);
+}
+
+else if (num1 > num2)
+{
+    Console.Write("max = ");
+    Console.Write(num1);
+}
+
+else
+{
+    Console.Write("Numbers are equal");
+}
