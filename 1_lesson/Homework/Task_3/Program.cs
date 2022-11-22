7d4c2cd165520bc8c8e83c5f0eb440e4bf7fb695
Sergey <smysatov@gmail.com>
2022-11-22 21:29:59 +0300

    Added HomeWork Task_3

---
+++ 1_lesson/Homework/Task_3/Program.cs
@@ -0,0 +1,15 @@
+﻿// Программа на вход принимает число и выдает, является ли
+// число четным (делится ли оно на 2 без остатка)
+
+Console.WriteLine("Enter number");
+
+int num = int.Parse(Console.ReadLine());
+    
+if (num % 2 == 0)
+{
+    Console.WriteLine("Even number");
+}
+else
+{
+    Console.WriteLine("Odd number");
+}
