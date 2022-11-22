42756751aa1c59f5e3f246ddc6dfa46478f897dd
Sergey <smysatov@gmail.com>
2022-11-22 12:36:41 +0300

    Added task 1.2

--- 1_lesson/1.2/Program.cs
+++ 1_lesson/1.2/Program.cs
@@ -1,20 +1,44 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Enter number"); int y = int.Parse(Console.ReadLine()); if ( y == 1)  {     Console.WriteLine("Понедельник"); } else if (y == 2) {     Console.WriteLine("Вторник"); } else if (y == 3) {     Console.WriteLine("Cреда"); } else if (y == 4)
+﻿// выдает название дня недели по заданному номеру
+
+Console.WriteLine("Enter number");
+
+int y = int.Parse(Console.ReadLine());
+
+if ( y == 1)
 {
-    Console.WriteLine("Четверг");
+    Console.WriteLine("Monday");
 }
+
+else if (y == 2)
+{
+    Console.WriteLine("Tuesday");
+}
+
+else if (y == 3)
+{
+    Console.WriteLine("Wednesday");
+}
+
+else if (y == 4)
+{
+    Console.WriteLine("Thursday");
+}
+
 else if (y == 5)
 {
-    Console.WriteLine("Пятница");
+    Console.WriteLine("Friday");
 }
+
 else if (y == 6)
 {
-    Console.WriteLine("Суббота");
+    Console.WriteLine("Saturday");
 }
+
 else if (y == 7)
 {
-    Console.WriteLine("Воскресенье");
+    Console.WriteLine("Sunday");
 }
+
 else
 {
     Console.WriteLine("error");
