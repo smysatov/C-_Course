d5553bf3f92b131d308c2ff969f91a02eef8e857
Sergey <smysatov@gmail.com>
2022-11-22 13:03:52 +0300

    Added task 1.4

--- 1_lesson/1.4/Program.cs
+++ 1_lesson/1.4/Program.cs
@@ -1,2 +1,6 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// Программа вычисления модуля числа
+
+Console.WriteLine("Enter number");
+var s = Console.ReadLine();
+int N = s == null ? 0 : int.Parse(s);
+Console.WriteLine($"{Math.Abs(N)}");
