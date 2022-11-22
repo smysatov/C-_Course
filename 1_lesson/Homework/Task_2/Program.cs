bd69424834f9cf6f31814656af26c79e058de072
Sergey <smysatov@gmail.com>
2022-11-22 16:11:40 +0300

    Added homework task 2

---
+++ 1_lesson/Homework/Task_2/Program.cs
@@ -0,0 +1,28 @@
+﻿// Принимает на вход три числа и выдает максимальное
+// из этих чисел
+
+Console.WriteLine("Enter first number");
+int num1 = int.Parse(Console.ReadLine());
+
+Console.WriteLine("Enter second number");
+int num2 = int.Parse(Console.ReadLine());
+
+Console.WriteLine("Enter third number");
+int num3 = int.Parse(Console.ReadLine());
+
+int max;
+
+max = num1;
+
+if (max < num2)
+{
+    max = num2;
+}
+if (max < num3)
+{
+    max = num3;
+}
+
+
+Console.Write("max = ");
+Console.Write(max);
