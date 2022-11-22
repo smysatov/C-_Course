bfefd43d38d93fd24c55856439dae96c929c0565
Sergey <smysatov@gmail.com>
2022-11-22 22:21:53 +0300

    Added HomeWork Task_4

---
+++ 1_lesson/Homework/Task_4/Program.cs
@@ -0,0 +1,13 @@
+﻿// Программа на вход принимает число (N), а на выходе
+// показывает все четные числа от 1 до N
+
+Console.WriteLine("Enter number");
+int N = int.Parse(Console.ReadLine());
+int x = 1;
+
+while (x<=N)
+{
+    if (x%2==0)
+    Console.Write(x+", ");
+    x++;
+}
