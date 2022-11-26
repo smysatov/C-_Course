985f9f4de97c41f1fee726172de37eba8c96520c
Sergey <smysatov@gmail.com>
2022-11-26 11:18:35 +0300

    Added 2.0

---
+++ 2_lesson/2.0/Program.cs
@@ -0,0 +1,14 @@
+﻿// Выводит случайное число на отрезке [10,99]
+// и показывает наибольшую цифру числа
+
+int Maxnumber(int num)
+{
+    Console.WriteLine(num);
+
+    if (num % 10 > num / 10)
+        return num % 10;
+    else
+        return num / 10;
+}
+
+Console.WriteLine(Maxnumber(new Random().Next(10, 100)));
