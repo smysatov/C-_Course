6fdb35b9976b8e31bd13289a11979c05cc4477b4
Sergey <smysatov@gmail.com>
2022-11-26 21:42:36 +0300

    Added Task_3

---
+++ 2_lesson/Homework/Task_3/Program.cs
@@ -0,0 +1,23 @@
+﻿//Выводит третью цифру заданного числа или сообщает,
+// что третьей цифры нет
+
+Console.WriteLine("Enter a number");
+int num = int.Parse(Console.ReadLine());
+
+if (num < 100)
+
+{
+    Console.WriteLine("The third digit does not exist");
+}
+
+else
+{
+    while (num > 1000)
+{
+    num = num / 10;
+}
+
+num = num % 10;
+
+Console.WriteLine($"Third digit is {num}");
+}
