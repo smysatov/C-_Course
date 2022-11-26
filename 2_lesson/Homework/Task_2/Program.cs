f61d5ef3f12169cb3bb2801c43ea6aab4568e4d2
Sergey <smysatov@gmail.com>
2022-11-26 20:55:40 +0300

    Added Task_2

--- 2_lesson/Homework/Task_2/Program.cs
+++ 2_lesson/Homework/Task_2/Program.cs
@@ -1,1 +1,10 @@
-﻿//Выводит случайное трехзначное число
+﻿//Выводит случайное трехзначное число и удаляет
+//вторую цифру этого числа
+
+int Num(int num)
+{
+    Console.WriteLine($"Random three digit number {num}");
+    Console.Write("Without second digit ");
+    return (num / 100) * 10 + num % 10;
+}
+Console.WriteLine(Num(new Random().Next(100, 1000)));
