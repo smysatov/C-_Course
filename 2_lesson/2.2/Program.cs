8b6f395a20832910ce8e7f8c7b7c2172fdd95119
Sergey <smysatov@gmail.com>
2022-11-26 12:17:30 +0300

    Added task 2.2

---
+++ 2_lesson/2.2/Program.cs
@@ -0,0 +1,17 @@
+﻿// На вход принимает 2 числа и выводит, является ли второе
+// число кратным первому. Если число 2 не кратно первому,
+// то программа выводит остаток от деления.
+
+Console.WriteLine("Write first number: ");
+int num1 = int.Parse(Console.ReadLine());
+Console.WriteLine("Write second number: ");
+int num2 = int.Parse(Console.ReadLine());
+
+string Cratno(int num1, int num2)
+{
+    if (num1 % num2 == 0)
+        return "multiple";
+    else
+        return $"non-multiple, modulo {num1%num2}";
+}
+Console.WriteLine(Cratno(num1,num2));
