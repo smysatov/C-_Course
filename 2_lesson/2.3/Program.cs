6cd37d73faca9547bd2b93d410d2ead15038c79b
Sergey <smysatov@gmail.com>
2022-11-26 17:01:44 +0300

    Added task 2.3

---
+++ 2_lesson/2.3/Program.cs
@@ -0,0 +1,14 @@
+﻿// Принимает на вход число и проверяет,
+// кратно ли оно одновременно 7 и 23!!!
+
+Console.WriteLine("Write a number: ");
+int num = int.Parse(Console.ReadLine());
+
+string Cratno(int num)
+{
+    if (num % 7 == 0 && num % 23 == 0)
+        return "yes";
+    else
+        return "no";    
+}
+Console.WriteLine(Cratno(num));
