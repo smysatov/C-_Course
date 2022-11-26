a74d3f3e6d8577ac040ee6a203f03b148c093f6b
Sergey <smysatov@gmail.com>
2022-11-26 17:20:05 +0300

    Homework Task_1

---
+++ 2_lesson/Homework/Task_1/Program.cs
@@ -0,0 +1,9 @@
+﻿//Принимает на вход трехзначное число
+// и на выходе показывает вторую цифру этого числа.
+
+Console.WriteLine("Write a three digit number: ");
+int num = int.Parse(Console.ReadLine());
+
+num = num / 10;
+num = num % 10;
+Console.WriteLine($"Second digit {num}");
