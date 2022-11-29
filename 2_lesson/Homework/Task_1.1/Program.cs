d7f04f206204143653b02d85cccfdc5ca04ac23a
Sergey <smysatov@gmail.com>
2022-11-29 14:15:30 +0300

    Added Task_1.1

---
+++ 2_lesson/Homework/Task_1.1/Program.cs
@@ -0,0 +1,11 @@
+﻿//Принимает на вход трехзначное число
+// и на выходе показывает вторую цифру этого числа.
+
+string NumSec(int num)
+{
+    if ((-1000 < num && num < 100) || (num < 1000 && num > 100))
+        return $"{num / 10 % 10}";
+    return "The number is not three-digit";
+}
+
+Console.WriteLine(NumSec(int.Parse(Console.ReadLine())));
