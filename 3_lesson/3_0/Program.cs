e265a7e470783333f232c93905f49540c0b355ef
Sergey <smysatov@gmail.com>
2022-11-29 21:05:27 +0300

    Task 3_0

---
+++ 3_lesson/3_0/Program.cs
@@ -0,0 +1,19 @@
+﻿// На вход принимает координаты точки (X и Y), причем
+//X или Y не равно 0 и выдает номер четверти плоскости,
+//в которой находится эта точка.
+
+void Quarters(int x, int y)
+{
+    if (x == 0 || y == 0)
+        Console.WriteLine("x=0 or y=0");
+    else if (x > 0 && y > 0)
+        Console.WriteLine("I");
+    else if (x > 0 && y < 0)
+        Console.WriteLine("IV");
+    else if (x < 0 && y > 0)
+        Console.WriteLine("II");
+    else if (x < 0 && y < 0)
+        Console.WriteLine("III");
+}
+
+Quarters(2, -6);
