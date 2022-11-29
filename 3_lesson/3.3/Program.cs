0637e5c1c7c4f4f7dde93af946492a76c9c29064
Sergey <smysatov@gmail.com>
2022-11-29 22:45:21 +0300

    Added 3.3

---
+++ 3_lesson/3.3/Program.cs
@@ -0,0 +1,24 @@
+﻿//Принимает на вход число (N) и выдает таблицу
+//квадратов чисел от 1 до N
+void Chet(int N)
+{
+    int i = 0;
+    double pere;
+    while (i < N)
+    {
+        i += 1;
+        if (i == (N))
+        {
+            pere = Math.Pow(i, 2);
+            Console.Write($"{pere}");
+        }
+        else
+        {
+            pere = Math.Pow(i, 2);
+            Console.Write($"{pere},");
+        }
+    }
+}
+Console.WriteLine("Enter a number");
+int chislo = int.Parse(Console.ReadLine());
+Chet(chislo);
