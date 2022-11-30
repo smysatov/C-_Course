0852c0988562dab9ffa043c647944ff1f8437d08
Sergey <smysatov@gmail.com>
2022-11-30 14:00:38 +0300

    Added Task_3

---
+++ 3_lesson/Homework/Task_3/Program.cs
@@ -0,0 +1,27 @@
+﻿//Принимает на вход число N и выдает таблицу кубов
+//чисел от 1 до N.
+
+void Cube(int Num)
+{
+    int i = 1;
+    double Kub;
+    while (i <= Num)
+    {
+        if (i < Num)
+        {
+            Kub = Math.Pow(i, 3);
+            Console.Write($"{Kub}, ");
+            i += 1;
+        }
+        else
+        {
+            Kub = Math.Pow(i, 3);
+            Console.Write($"{Kub}");
+            i += 1;
+        }
+    }
+}
+
+Console.WriteLine("Enter a Number");
+int Num = int.Parse(Console.ReadLine());
+Cube(Num);
