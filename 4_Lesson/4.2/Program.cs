7335cf351c0053fb980f422f31e927fa0397197b
Sergey <smysatov@gmail.com>
2022-12-03 21:29:38 +0300

    Added 4.2

---
+++ 4_Lesson/4.2/Program.cs
@@ -0,0 +1,15 @@
+﻿// Принимает на вход число и выдает количество цифр в числе.
+
+int Sum(int num)
+{
+    int i = 0;
+    while (num > 0)
+    {
+        num = num / 10;
+        i++;
+    }
+    return i;
+}
+
+Console.Write("Введите число -> ");
+Console.WriteLine($"Количество цифр в числе -> {Sum(int.Parse(Console.ReadLine()))}");
