fca1e7b0ace42853451f565ecc2a16e9c98fa54f
Sergey <smysatov@gmail.com>
2022-12-04 16:19:57 +0300

    Added 4.3

---
+++ 4_Lesson/4.3/Program.cs
@@ -0,0 +1,16 @@
+﻿//Выводит массив из 8 элементов, заполненный нулями
+//и единицами в случайном порядке
+
+void FillArray(int n)
+{
+    Console.Write("Вот что получилось: ");
+    int[] array = new int[n];
+    for (int i = 0; i < n; i++)
+    {
+        array[i] = new Random().Next(0, 10);
+        Console.Write($"{array[i]}, ");
+    }
+}
+
+Console.Write("введите количество элементов массива N -> ");
+FillArray(int.Parse(Console.ReadLine()));
