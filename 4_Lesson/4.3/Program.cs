9892315ef302dbc81b97d77211265815c103f313
Sergey <smysatov@gmail.com>
2022-12-04 23:25:05 +0300

    Added Task_3

--- 4_Lesson/4.3/Program.cs
+++ 4_Lesson/4.3/Program.cs
@@ -7,7 +7,7 @@ void FillArray(int n)
     int[] array = new int[n];
     for (int i = 0; i < n; i++)
     {
-        array[i] = new Random().Next(0, 10);
+        array[i] = new Random().Next(0, 99);
         Console.Write($"{array[i]}, ");
     }
 }
---
+++ 4_Lesson/Homework/Task_3/Program.cs
@@ -0,0 +1,29 @@
+﻿//задает массив из N элементов случайными числами и выводит
+//их на экран. Оформите заполнение массива и вывод в виде
+//функции(для следующих задач).
+
+void FillArray(int[] array)
+{
+    int length = array.Length;
+    for (int i = 0; i < length; i++)
+    {
+        array[i] = new Random().Next(-99, 99);
+    }
+}
+
+void PrintArray(int[] array)
+{
+    int length = array.Length;
+    Console.Write("Случайный Массив N: ");
+    for (int i = 0; i < length; i++)
+    {
+        Console.Write($"{array[i]}, ");
+    }
+}
+
+Console.Write("введите количество элементов массива N: ");
+int N = int.Parse(Console.ReadLine());
+
+int[] array = new int[N];
+FillArray(array);
+PrintArray(array);
