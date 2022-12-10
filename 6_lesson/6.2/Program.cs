4db560e043f4ca101e8bbd645ec4348d50276569
Sergey <smysatov@gmail.com>
2022-12-10 23:08:19 +0300

    Added 6_lesson

---
+++ 6_lesson/6.2/Program.cs
@@ -0,0 +1,15 @@
+﻿// Программа преобразовывает десятичное число в двоичное
+
+string binary(int num)
+{
+    string sum = "";
+    while (num > 0)
+    {
+        sum = num % 2 + sum;
+        num = num / 2;
+    }
+    return sum;
+}
+Console.Write("Введите десятичное число, ");
+int num = int.Parse(Console.ReadLine());
+Console.WriteLine($"Двоичное число, {binary(num)}");
