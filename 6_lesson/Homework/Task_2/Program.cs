a96e27ba7d00336d910e203d61a1762b077f31f5
Sergey <smysatov@gmail.com>
2022-12-11 17:59:02 +0300

    Added Task_2

---
+++ 6_lesson/Homework/Task_2/Program.cs
@@ -0,0 +1,25 @@
+﻿//Программа, которая найдет точку пересечения двух прямых,
+//заданных уравнениями y = k1 * x +b1, y = k2*x + b2; значения
+//b1, k1, b2 и k2 задаются пользователем.
+
+
+Console.WriteLine("Введите значение b1");
+double b1 = double.Parse(Console.ReadLine());
+
+Console.WriteLine("Введите значение k1");
+double k1 = double.Parse(Console.ReadLine());
+
+Console.WriteLine("Введите значение b2");
+double b2 = double.Parse(Console.ReadLine());
+
+Console.WriteLine("Введите значение k2");
+double k2 = double.Parse(Console.ReadLine());
+
+void Tochka(double b1, double k1, double b2, double k2)
+{
+    double x = (b1 - b2) / (k2 - k1);
+    double y = k2 * x + b2;
+    Console.WriteLine($"Точка пересечения, ({x}, {y})");
+}
+
+Tochka(b1, k1, b2, k2);
