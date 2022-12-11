a0853b1ab63242bc4362f06f19752c9b11c0e8d3
Sergey <smysatov@gmail.com>
2022-12-11 18:41:41 +0300

    Added Task_1

---
+++ 6_lesson/Homework/Task_1/Program.cs
@@ -0,0 +1,22 @@
+﻿//Пользователь вводит с клавиатуры М чисел.
+//Посчитайте, сколько чисел больше 0 ввел пользователь.
+
+Console.WriteLine("Сколько чисел вы хотите ввести?");
+int M = int.Parse(Console.ReadLine());
+
+int Positive(int M)
+{
+    int sum = 0;
+    for (int i = 0; i < M; i++)
+    {
+        Console.WriteLine("Введите число");
+        int num = int.Parse(Console.ReadLine());
+        if (num > 0)
+        {
+            sum++;
+        }
+    }
+    return sum;
+}
+
+Console.WriteLine($"Количество положительных чисел: {Positive(M)}");
