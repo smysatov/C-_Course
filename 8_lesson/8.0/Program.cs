2e83d02492e8150fd00fa98ce29b947ed36aaba6
Sergey <smysatov@gmail.com>
2022-12-16 20:03:28 +0300

    Added 8_lesson

---
+++ 8_lesson/8.0/Program.cs
@@ -0,0 +1,48 @@
+﻿//Задайте двумерный массив. Напишите программу, которая
+//поменяет местами первую и последную строку массива.
+
+int[,] FillArray(int rows, int columns, int min, int max)
+{
+    int[,] arr = new int[rows, columns];
+
+    for (int i = 0; i < rows; i++)
+        for (int j = 0; j < columns; j++)
+            arr[i, j] = new Random().Next(min, max);
+    ;
+    return arr;
+}
+
+void PrintArray(int[,] arr)
+{
+    int row_size = arr.GetLength(0);
+    int column_size = arr.GetLength(1);
+
+    for (int i = 0; i < row_size; i++)
+    {
+        for (int j = 0; j < column_size; j++)
+            Console.Write($" {arr[i, j], 4} ");
+        Console.WriteLine();
+    }
+    Console.WriteLine();
+}
+
+Console.Write("Введите количество строк: ");
+int row = int.Parse(Console.ReadLine());
+Console.Write("Введите количество столбцов: ");
+int column = int.Parse(Console.ReadLine());
+Console.Write("Введите минимальное значение элемента массива: ");
+int min = int.Parse(Console.ReadLine());
+Console.Write("Введите максимальное значение элемента массива: ");
+int max = int.Parse(Console.ReadLine());
+
+int[,] arr_1 = FillArray(row, column, min, max);
+PrintArray(arr_1);
+
+void ChangeArray(int[,] arr)
+{
+    for (int i = 0; i < arr.GetLength(1); i++)
+        (arr[0, i], arr[arr.GetLength(0) - 1, i]) = (arr[arr.GetLength(0) - 1, i], arr[0, i]);
+}
+
+ChangeArray(arr_1);
+PrintArray(arr_1);
