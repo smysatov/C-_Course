872dd1815aaf033cb53929ddf6dfe440445daf9e
Sergey <smysatov@gmail.com>
2022-12-22 12:59:29 +0300

    Added Task_2

---
+++ 9_Lesson/Homework/Task_2/Program.cs
@@ -0,0 +1,18 @@
+﻿//Задайте значения M и N. Напишите программу, которая найдет
+//сумму натуральных элементов в промежутке от M до N
+//помощью рекурсии.
+
+int Summa(int M, int N)
+{
+    if (M == N)
+        return M;
+    return Summa(M + 1, N) + M;
+}
+
+Console.Write("Введите число M: ");
+int M = int.Parse(Console.ReadLine());
+
+Console.Write("Введите число N: ");
+int N = int.Parse(Console.ReadLine());
+
+Console.Write($"Сумма эл. в промежутке от M до N: {Summa(M, N)}");
